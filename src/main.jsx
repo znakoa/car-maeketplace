e2d338bd3a1e8ea6400962b509b688b04473134b
znakoa <zhougui@zjwykjyxgs13.wecom.work>
2024-09-06 14:04:59 +0800

    统一前缀

--- src/main.jsx
+++ src/main.jsx
@@ -8,40 +8,46 @@ import Home from './home'
 import Contact from './contact'
 import Profile from './profile'
 import AddListing from './add-listing'
-import SarchByCategoty from "@/search/[category]/index.jsx";
-import SarchByOptions from "@/search/index.jsx";
-import ListDetails from "@/list-details/[id]/index.jsx";
+import SarchByCategoty from '@/search/[category]/index.jsx'
+import SarchByOptions from '@/search/index.jsx'
+import ListDetails from '@/list-details/[id]/index.jsx'
 // import { Toaster } from './components/ui/sonner'
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/contact',
+      element: <Contact />,
+    },
+    {
+      path: '/profile',
+      element: <Profile />,
+    },
+    {
+      path: 'add-listing',
+      element: <AddListing />,
+    },
+    {
+      path: '/search',
+      element: <SarchByOptions />,
+    },
+    {
+      path: '/search/:category',
+      element: <SarchByCategoty />,
+    },
+    {
+      path: '/listing-details/:id',
+      element: <ListDetails />,
+    },
+  ],
   {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/contact',
-    element: <Contact />,
-  },
-  {
-    path: '/profile',
-    element: <Profile />,
-  },
-  {
-    path: 'add-listing',
-    element: <AddListing />,
-  },
-  {
-    path:'/search',
-    element:<SarchByOptions/>
-  },
-  {
-    path:'/search/:category',
-    element:<SarchByCategoty/>
-  },{
-    path:'/listing-details/:id',
-    element:<ListDetails/>
+    basename: '/car-maeketplace', // 添加统一前缀
   }
-])
+)
 // Import your publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
