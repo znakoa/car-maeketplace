370e5b5cc3f0f81de06388d92412a71aa3043ec1
znakoa <59088116+znakoa@users.noreply.github.com>
2024-09-06 05:42:51 +0000

    Deploying to gh-pages from @ znakoa/car-maeketplace@d7ed17b2ac2c4e3d453de2454491aa310f5071a8 🚀

--- drizzle.config.js
+++
@@ -1,8 +0,0 @@
-/** @type { import("drizzle-kit").Config } */
-export default {
-  schema: './configs/schema.js',
-  dialect: 'postgresql',
-  dbCredentials: {
-    url: 'postgresql://neondb_owner:OPsqT9JSKLa6@ep-shy-sea-a5ap8l1y.us-east-2.aws.neon.tech/car-maeketplace?sslmode=require',
-  },
-}
