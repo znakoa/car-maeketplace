683c619a853702d7a9947926d0b8c7e353835736
znakoa <zhougui@zjwykjyxgs13.wecom.work>
2024-09-06 13:46:04 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
